604f6d89484a5d783e10aa01b8c140dfde751599
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-07-03 00:37:58 -0400

    Invite memebers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require twitter/bootstrap
+//= require twitter/bootstrap/alert
 //= require activestorage
 //= require_tree .
